f6b223007159cf915679ba99013ad2f20b1ef0c5
Charlie Crocker <cscrocker84@gmail.com>
2016-12-07 08:27:38 -0600

    added 2 new XMLHttpRequests

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -13,20 +13,47 @@ var salesData;
 //function that will parse product info and department to the page
 var productsCat = function () {
 	pageItems = "";
-	for (var i = 0; < products.length; i++) {
+	for (var i = 0; i < products.length; i++) {
 		//assigns department
 		if (products[i].category_id === 1) {
 			products[i].category = "Apparel";
 	}	else if (products[i].category_id === 2) {
 			products[i].category = "Furniture";
-	}	else if ((products[i].category_id === 3) {
+	}	else if (products[i].category_id === 3) {
 			products[i].category = "Household";
 	}
 
 //assign product name, department, and price to the div
 	pageItems += `<h3>${products[i].name}</h3>
-					<p>In the ${products[i].category} department</p>
+					<p>In the ${products[i].category} Department</p>
 					<p class="${products[i].category_id}">Price is: <span class="price">${products[i].price}</span></p>`;
 	//post
 	divItems.innerHTML = pageItems;
 }
+
+//function to parse JSON data for items and load into page
+var getProducts = function (e) {
+	products = JSON.parse(e.target.responseText);
+	productsCat();
+}
+
+//function to parse JSON data for seasons
+var getSales = function (e) {
+	salesData = JSON.parse(e.target.responseText);
+}
+
+//get list of products from products.json by file load completion
+var storeItems = new XMLHttpRequest();
+storeItems.addEventListener("load", getSales);
+storeItems.open("GET", "products.json");
+storeItems.send();
+
+//get seasonal discount from categories.json by file load completion
+var storeCat = new XMLHttpRequest();
+storeCat.addEventListener("load", getSales);
+storeCat.open("GET", "categories.json");
+storeCat.send();
+console.log(storeCat);
+}
+
+//All Prices should auto-update with the discount
